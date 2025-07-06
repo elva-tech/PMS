61fc75b9f6e62d18af2d75a23fe7c24a3c8a91cb
Abhiabee <abhisheku.1si18ec006@gmail.com>
2025-07-06 23:18:00 +0530

    Configured for GitHub Pages deployment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Landingpage from "./Pages/Landingpage";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import Layout from "./containers/Layout";
 
 function App() {
